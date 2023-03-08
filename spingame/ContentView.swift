0efb8e3c6d0cdf435d15499ad859bbe918a4f158
hank <taiwanhank0908@gmail.com>
2023-03-08 13:01:31 +0800

    Initial Commit

---
+++ spingame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spingame
+//
+//  Created by User19 on 2023/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spingame/spingameApp.swift
@@ -0,0 +1,17 @@
+//
+//  spingameApp.swift
+//  spingame
+//
+//  Created by User19 on 2023/3/8.
+//
+
+import SwiftUI
+
+@main
+struct spingameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
